4b18e1be729a72eb9f488f6c7437fb88c03615e9
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-03-03 14:01:11 -0500

    Trying to understand setup tools and Pyinstaller

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+
+version = "0.0.1"
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+setup(
+    name="Jpoc",
+    packages=["jpoc"],
+    entry_points={
+
+        "console_scripts": ['jpoc = jpoc.Jpoc:main']
+
+    },
+    version=version,
+    description="Python command line application bare bones.",
+    long_description=long_descr,
+    author="cah-cesar-medrano",
+    author_email="cesar.medrano@cardinalhealth.com",
+)
