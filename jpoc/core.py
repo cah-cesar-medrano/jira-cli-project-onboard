925f372638837ec30c8f28d153fea88f21c2f488
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-03-04 13:23:36 -0500

    Tested login, now to properly structure the cli and flow

--- jpoc/core.py
+++ jpoc/core.py
@@ -1,6 +1,7 @@
 """ Program Entry Point for __main__.py"""
 
 import sys
+from getpass import getpass, getuser
 from jpoc.logic import title_bar, jira_login, User
 
 
@@ -8,10 +9,13 @@ def main():
     title_bar()
 
     try:
-        user_name = input("\tUser Name: \n")
-        password = input("\tPassword: \n")
-        server = input("\tServer Url: \n")
+        user_name = getuser()  # input("\tUser Name: ")
+        print("\t Hello! {}!".format(user_name))
+        password = getpass(prompt="\t Password: ")
+        server = input("\tServer Url: ")
         jira = jira_login(User(user_name, password, server))
+        title_bar()
+        print(jira.user(user_name))
     except Exception as e:
         print('Login Failed\n')
         print(e)
